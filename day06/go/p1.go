9130fb485415c4e51605192d62d4a98c5d446be9
Jonas <26679346+jnzd@users.noreply.github.com>
2021-12-07 00:17:43 +0100

    playing around with go

--- day06/go/p1.go
+++ day06/go/p1.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,5 +16,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(content))
+	var inputString string = string(content)
+	// split inputString on ','
+	var input []string = strings.Split(inputString, ",")
+	var s string = "123"
+	// cast string s int i
+	i, err_strconv := strconv.Atoi(s)
+
+	if err_strconv != nil {
+		log.Fatal(err_strconv)
+	}
+
+	fmt.Println(i)
+	var inputInts []int = make([]int, len(input))
+	fmt.Println(inputInts)
+
+	//print input array
+	fmt.Println(input)
+
+	// fmt.Println(inputString)
+
 }
