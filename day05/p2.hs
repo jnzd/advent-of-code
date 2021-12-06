b3385be76f0fd1f66008be18185f0c932635194f
Jonas <26679346+jnzd@users.noreply.github.com>
2021-12-06 11:10:41 +0100

    day05 simplified

--- day05/p2.hs
+++ day05/p2.hs
@@ -23,7 +23,6 @@
 
 import Data.List
 
--- format lines of form "x1,y1 -> x2,y2" into ((x1::Int,y1::Int), (x2::Int,y2::Int))
 formatInput :: String -> ((Int, Int), (Int, Int))
 formatInput s =
     let x1 = read $ takeWhile (/= ',') s
@@ -40,19 +39,19 @@ pointsCovered ((x1, y1), (x2, y2))
     | x1 == x2  = [(x1, y) | y <- [min y1 y2 .. max y1 y2]]
     | y1 == y2  = [(x, y1) | x <- [min x1 x2 .. max x1 x2]]
     | x1 <= x2 && y1 <= y2 = 
-        [(min x1 x2 + i, min y1 y2 + i) | i <- [0 .. max x1 x2 - min x1 x2]]
+        [(min x1 x2 + i, min y1 y2 + i) | i <- [0 .. range]]
     | x1 <= x2 && y1 >= y2 = 
-        [(min x1 x2 + i, max y1 y2 - i) | i <- [0 .. max x1 x2 - min x1 x2]]
+        [(min x1 x2 + i, max y1 y2 - i) | i <- [0 .. range]]
     | x1 >= x2 && y1 <= y2 = 
-        [(max x1 x2 - i, min y1 y2 + i) | i <- [0 .. max x1 x2 - min x1 x2]]
+        [(max x1 x2 - i, min y1 y2 + i) | i <- [0 .. range]]
     | x1 >= x2 && y1 >= y2 = 
-        [(max x1 x2 - i, max y1 y2 - i) | i <- [0 .. max x1 x2 - min x1 x2]]
+        [(max x1 x2 - i, max y1 y2 - i) | i <- [0 .. range]]
     | otherwise = []
+    where range = abs $ x1 - x2
 
 main :: IO ()
 main = do
     input <- readFile "in.txt"
     let formattedVents = map formatInput $ lines input
-
     let coveredPoints = filter (\x -> length x > 1) $ groupBy (\(x1,y1) (x2,y2) -> x1 == x2 && y1 == y2) $ sort $ concatMap pointsCovered formattedVents
     print $ length coveredPoints
