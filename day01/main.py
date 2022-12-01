7ca1c81d990aa124c8251b2a289e4a207529126f
Jonas Degelo <jonas.degelo@gmail.com>
2022-12-01 20:34:10 +0100

    day01 part 2

--- day01/main.py
+++ day01/main.py
@@ -1,4 +1,4 @@
-def solve(input_path):
+def solve_part1(input_path):
     with open(input_path) as f:
         data = [d.splitlines() for d in f.read().split('\n\n')]
         data = [[int(d) for d in dd] for dd in data]
@@ -6,5 +6,20 @@ def solve(input_path):
         return max(data)
 
 
-print(solve('./sample.txt'))
-print(solve('./in.txt'))
+def solve_part2(input_path):
+    with open(input_path) as f:
+        data = [d.splitlines() for d in f.read().split('\n\n')]
+        data = [[int(d) for d in dd] for dd in data]
+        data = [sum(d) for d in data]
+        res = 0
+        for _ in range(3):
+            top = max(data)
+            data.remove(top)
+            res += top
+        return res
+
+
+print(solve_part1('./sample.txt'))
+print(solve_part2('./sample.txt'))
+print(solve_part1('./in.txt'))
+print(solve_part2('./in.txt'))
