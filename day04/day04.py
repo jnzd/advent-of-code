2a5b90cd47e41571aa3b2d577be7c0b4fae857ce
Jonas Degelo <jonas.degelo@gmail.com>
2022-12-04 12:42:17 +0100

    reformatted long lines for readability

--- day04/day04.py
+++ day04/day04.py
@@ -1,12 +1,17 @@
 def part_one(input):
     segments = get_segments(input)
-    fully_contained = [seg for seg in segments if seg[0][0] <= seg[1][0] and seg[0][1] >= seg[1][1] or seg[0][0] >= seg[1][0] and seg[0][1] <= seg[1][1]]
+    fully_contained = [seg for seg in segments if seg[0][0] <= seg[1][0] and 
+                                                  seg[0][1] >= seg[1][1] or 
+                                                  seg[0][0] >= seg[1][0] and 
+                                                  seg[0][1] <= seg[1][1]]
     return len(fully_contained)
 
 def part_two(input):
     segments = get_segments(input)
-    overlap = [seg for seg in segments if seg[0][0] in range(seg[1][0], seg[1][1] + 1) or seg[0][1] in range(seg[1][0], seg[1][1] + 1) or 
-                                          seg[1][0] in range(seg[0][0], seg[0][1] + 1) or seg[1][1] in range(seg[0][0], seg[0][1] + 1)]
+    overlap = [seg for seg in segments if seg[0][0] in range(seg[1][0], seg[1][1] + 1) or 
+                                          seg[0][1] in range(seg[1][0], seg[1][1] + 1) or 
+                                          seg[1][0] in range(seg[0][0], seg[0][1] + 1) or 
+                                          seg[1][1] in range(seg[0][0], seg[0][1] + 1)]
     return len(overlap)
 
 def get_segments(input):
