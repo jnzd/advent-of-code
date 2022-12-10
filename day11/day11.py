6ba7f644619bd1f9b24459e890e043c4e63cabf6
Jonas Degelo <jonas.degelo@gmail.com>
2022-12-10 12:47:03 +0100

    day11 template

---
+++ day11/day11.py
@@ -0,0 +1,11 @@
+def part_one(input):
+    return 0
+
+def part_two(input):
+    return 0
+
+# with open('./sample.txt') as f:
+with open('./in.txt') as f:
+    print(part_one(f.read().splitlines()))
+    print(part_two(f.read().splitlines()))
+    
