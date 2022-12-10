a55cd02fc27e6a98446c6b608ec46ddbc9574ff3
Jonas Degelo <jonas.degelo@gmail.com>
2022-12-10 12:40:43 +0100

    day10 part 1 and 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,11 +1,37 @@
 def part_one(input):
-    return 0
+    trace = calc_trace(input)
+    return sum([i * trace[i] for i in range(20, len(trace), 40)])
+
+def calc_trace(input):
+    x = 1
+    trace = [x]
+    input = [[a[0], int(a[1])] if len(a) == 2 else a[0] for a in [line.split(' ') for line in input]]
+    for cmd in input:
+        trace.append(x)
+        if cmd[0] == 'addx':
+            trace.append(x)
+            x += cmd[1]
+    return trace
 
 def part_two(input):
-    return 0
+    trace = calc_trace(input)
+    crt = [['|' for _ in range(42)] for _ in range(8)]
+    for i in range(1,7):
+        for j in range(1,41):
+            k = (i-1)*40 + j
+            if (j-1) in range(trace[k]-1, trace[k]+2):
+                # crt[i][j] = '#'
+                crt[i][j] = '█'
+            else:
+                # crt[i][j] = '.'
+                crt[i][j] = ' '
+    for i in range(len(crt)):
+        print(''.join(crt[i]))
+    return crt
 
-with open('./sample.txt') as f:
-# with open('./in.txt') as f:
+# with open('./sample-small.txt') as f:
+# with open('./sample.txt') as f:
+with open('./in.txt') as f:
     input = f.read().splitlines()
     print(part_one(input))
-    print(part_two(input))
+    part_two(input)
