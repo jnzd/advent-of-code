af36066f46abf832d506e0599ea608ae96bc7404
Jonas Degelo <jonas.degelo@gmail.com>
2022-12-11 20:08:47 +0100

    day12 template

---
+++ day12/day12.py
@@ -0,0 +1,10 @@
+def part_one(input):
+    return 0
+
+def part_two(input):
+    return 0
+
+with open('./in.txt') as f:
+    input = f.read()
+    print(part_one(input))
+    print(part_two(input))
