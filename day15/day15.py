2b3e51ecd5412267fe575ffee5b3a9311d2c7388
Jonas Degelo <jonas.degelo@gmail.com>
2022-12-14 17:23:31 +0100

    day15 template

---
+++ day15/day15.py
@@ -0,0 +1,11 @@
+def part_one(x):
+    return 0
+
+def part_two(x):
+    return 0
+
+with open('./in.txt') as f:
+# with open('./sample.txt') as f:
+    input = f.read()
+    print(part_one(input))
+    print(part_two(input))
